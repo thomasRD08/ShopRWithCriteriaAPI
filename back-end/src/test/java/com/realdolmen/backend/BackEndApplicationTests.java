c57640e5924f49513aa7c5981a0551e71e249048
karelrd <karel.debruyne@realdolmen.com>
2019-01-25 17:26:37 +0100

    Remove whitespace

--- back-end/src/test/java/com/realdolmen/backend/BackEndApplicationTests.java
+++ back-end/src/test/java/com/realdolmen/backend/BackEndApplicationTests.java
@@ -8,10 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BackEndApplicationTests {
-
     @Test
     public void contextLoads() {
     }
-
 }
-
