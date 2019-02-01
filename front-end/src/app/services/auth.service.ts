b99d5471faac6098e83bd88b9a205131f445640d
karelrd <karel.debruyne@realdolmen.com>
2019-02-01 17:07:36 +0100

    Remove import

--- front-end/src/app/services/auth.service.ts
+++ front-end/src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {User} from "../models/user";
-import {ObjectUnsubscribedError, Observable, Subject} from "rxjs";
+import {Observable, Subject} from "rxjs";
 import {GlobalConstant} from "../components/shared/global-constant";
 
 @Injectable({
