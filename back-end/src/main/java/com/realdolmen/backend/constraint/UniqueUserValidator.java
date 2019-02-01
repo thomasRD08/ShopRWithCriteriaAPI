b382e172acc27d873a153f89ae156b6732eff95f
karelrd <karel.debruyne@realdolmen.com>
2019-02-01 14:16:08 +0100

    Create unique user constraint

---
+++ back-end/src/main/java/com/realdolmen/backend/constraint/UniqueUserValidator.java
@@ -0,0 +1,26 @@
+package com.realdolmen.backend.constraint;
+
+import com.realdolmen.backend.domain.User;
+import com.realdolmen.backend.repository.UserRepository;
+import lombok.AllArgsConstructor;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Optional;
+
+@AllArgsConstructor
+public class UniqueUserValidator implements ConstraintValidator<UniqueUser, String> {
+    private final UserRepository userRepository;
+
+    @Override
+    public void initialize(UniqueUser constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(String username, ConstraintValidatorContext context) {
+        Optional<User> user = userRepository.findByUsername(username);
+
+        return !user.isPresent();
+    }
+}
