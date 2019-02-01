e52d3ef07ec9fd52e70d986ac8b03cdb2984f3a5
karelrd <karel.debruyne@realdolmen.com>
2019-02-01 14:17:32 +0100

    Create CrudServiceImpl

---
+++ back-end/src/main/java/com/realdolmen/backend/service/CrudServiceImpl.java
@@ -0,0 +1,39 @@
+package com.realdolmen.backend.service;
+
+import com.realdolmen.backend.advice.exception.NotFoundException;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
+public abstract class CrudServiceImpl<T, PK> implements CrudService<T, PK> {
+    private final Class<T> entityClass;
+
+    @SuppressWarnings("unchecked")
+    public CrudServiceImpl() {
+        entityClass = (Class<T>) ((ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0];
+    }
+
+    abstract protected JpaRepository<T, PK> getRepository();
+
+    @Override
+    public T save(T entity) {
+        return getRepository().save(entity);
+    }
+
+    @Override
+    public T findById(PK primaryKey) {
+        return getRepository().findById(primaryKey)
+                .orElseThrow(() -> new NotFoundException("Primary key not found for " + entityClass.getName() + ": " + primaryKey));
+    }
+
+    @Override
+    public List<T> findAll() {
+        return getRepository().findAll();
+    }
+
+    @Override
+    public void delete(T entity) {
+        getRepository().delete(entity);
+    }
+}
