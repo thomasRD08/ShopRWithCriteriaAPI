88bb2982dcdda6f3c904774ba3626a1979780192
karelrd <karel.debruyne@realdolmen.com>
2019-02-04 10:42:59 +0100

    Fix UserServiceImpl

--- back-end/src/main/java/com/realdolmen/backend/service/UserServiceImpl.java
+++ back-end/src/main/java/com/realdolmen/backend/service/UserServiceImpl.java
@@ -5,17 +5,22 @@ import com.realdolmen.backend.domain.User;
 import com.realdolmen.backend.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class UserServiceImpl extends CrudServiceImpl<User, Long> implements UserService {
-    @Getter
-    private final UserRepository repository;
+    private final UserRepository userRepository;
+
+    @Override
+    protected JpaRepository<User, Long> getRepository() {
+        return userRepository;
+    }
 
     @Override
     public User findByUsername(String username) {
-        return repository.findByUsername(username)
+        return userRepository.findByUsername(username)
                 .orElseThrow(() -> new NotFoundException("User does not exist"));
     }
 }
