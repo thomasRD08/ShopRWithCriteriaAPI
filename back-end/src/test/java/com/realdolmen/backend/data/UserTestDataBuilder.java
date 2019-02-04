dc314f615d9aeb3a16b62905a4a7caea3cf8f8b1
karelrd <karel.debruyne@realdolmen.com>
2019-02-04 17:25:23 +0100

    Fix builder

--- back-end/src/test/java/com/realdolmen/backend/data/UserTestDataBuilder.java
+++ back-end/src/test/java/com/realdolmen/backend/data/UserTestDataBuilder.java
@@ -4,17 +4,17 @@ import com.realdolmen.backend.domain.User;
 import com.realdolmen.backend.dto.UserDto;
 
 public class UserTestDataBuilder {
-    public static User.Builder buildUserKarel() {
+    public static User.UserBuilder buildUserKarel() {
         return User.builder()
                 .username("Karel");
     }
 
-    public static User.Builder buildUserGuust() {
+    public static User.UserBuilder buildUserGuust() {
         return User.builder()
                 .username("Guust");
     }
 
-    public static UserDto.Builder buildUserDtoKarel() {
+    public static UserDto.UserDtoBuilder buildUserDtoKarel() {
         return UserDto.builder()
                 .username("Karel");
     }
