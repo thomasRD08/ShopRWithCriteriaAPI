2ad2d26fd512951cadf76ee181add44e15be2841
karelrd <karel.debruyne@realdolmen.com>
2019-02-04 15:39:09 +0100

    Add TODO

--- back-end/src/test/java/com/realdolmen/backend/config/CustomValidatorConfig.java
+++ back-end/src/test/java/com/realdolmen/backend/config/CustomValidatorConfig.java
@@ -4,6 +4,7 @@ import com.realdolmen.backend.constraint.UniqueUserValidator;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+//TODO: Delete if not used...
 @TestConfiguration
 @ComponentScan(basePackageClasses = UniqueUserValidator.class)
 public class CustomValidatorConfig {
