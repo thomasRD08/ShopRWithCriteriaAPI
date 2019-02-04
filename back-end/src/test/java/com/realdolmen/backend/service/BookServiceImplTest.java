1eb15f6bb71c5e4ded8165ea32e32fc5a1fbdae4
karelrd <karel.debruyne@realdolmen.com>
2019-02-04 17:26:05 +0100

    Test Product and Book services

---
+++ back-end/src/test/java/com/realdolmen/backend/service/BookServiceImplTest.java
@@ -0,0 +1,40 @@
+package com.realdolmen.backend.service;
+
+import com.realdolmen.backend.domain.Book;
+import com.realdolmen.backend.domain.Fiction;
+import com.realdolmen.backend.domain.NonFiction;
+import com.realdolmen.backend.repository.BookRepository;
+import com.realdolmen.backend.service.impl.BookServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class BookServiceImplTest {
+    @InjectMocks
+    private BookServiceImpl bookService;
+    @Mock
+    private BookRepository bookRepository;
+
+    @Test
+    public void testShouldFindAllBooks() {
+        List<Book> expectedBooks = new ArrayList<>();
+        expectedBooks.add(new Fiction());
+        expectedBooks.add(new NonFiction());
+
+        when(bookRepository.findAll()).thenReturn(expectedBooks);
+
+        List<Book> foundBooks = bookService.findAll();
+
+        verify(bookRepository, times(1)).findAll();
+        assertEquals(2, foundBooks.size());
+    }
+}
