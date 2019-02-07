ca2e71f479d650c6d89a309ef446b5af9d6c30f5
karelrd <karel.debruyne@realdolmen.com>
2019-02-07 17:06:17 +0100

    Create lp controller

---
+++ back-end/src/test/java/com/realdolmen/backend/controller/LpControllerTest.java
@@ -0,0 +1,29 @@
+package com.realdolmen.backend.controller;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+@Ignore
+public class LpControllerTest {
+//    TODO: Write tests
+
+    @Test
+    public void createLp() {
+    }
+
+    @Test
+    public void getLpById() {
+    }
+
+    @Test
+    public void testShouldGetAllLps() {
+    }
+
+    @Test
+    public void updateLp() {
+    }
+
+    @Test
+    public void deleteLp() {
+    }
+}
