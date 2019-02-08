5c564e5a63d191b7e553411a625b88b2dfd2fee7
karelrd <karel.debruyne@realdolmen.com>
2019-02-08 13:48:32 +0100

    Add new overview components to overview module

--- front-end/src/app/components/overview/overview.module.ts
+++ front-end/src/app/components/overview/overview.module.ts
@@ -1,18 +1,48 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {ProductsComponent} from "./products/products.component";
 import {SharedModule} from "../shared/shared.module";
+import {ProductService} from "../../services/product.service";
+import {BooksComponent} from './books/books.component';
+import {BookService} from "../../services/book.service";
+import {LpsComponent} from './lps/lps.component';
+import {GamesComponent} from './games/games.component';
+import {LpService} from "../../services/lp.service";
+import {GameService} from "../../services/game.service";
+import {FictionService} from "../../services/fiction.service";
+import {NonFictionService} from "../../services/non-fiction.service";
+import {FictionComponent} from './fiction/fiction.component';
+import {NonFictionComponent} from './non-fiction/non-fiction.component';
 
 @NgModule({
   declarations: [
-    ProductsComponent
+    ProductsComponent,
+    BooksComponent,
+    LpsComponent,
+    GamesComponent,
+    FictionComponent,
+    NonFictionComponent
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports: [
-    ProductsComponent
+    ProductsComponent,
+    BooksComponent,
+    LpsComponent,
+    GamesComponent,
+    FictionComponent,
+    NonFictionComponent
+  ],
+  providers: [
+    ProductService,
+    BookService,
+    LpService,
+    GameService,
+    FictionService,
+    NonFictionService
   ]
 })
-export class OverviewModule { }
+export class OverviewModule {
+}
