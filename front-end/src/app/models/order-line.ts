a1dffb54eca0530abadc11a339d62c6bfd0fd0c2
karelrd <karel.debruyne@realdolmen.com>
2019-02-18 17:02:54 +0100

    Create order-line service and model

---
+++ front-end/src/app/models/order-line.ts
@@ -0,0 +1,6 @@
+import {Product} from "./product";
+
+export class OrderLine {
+  public product: Product;
+  public amount: number;
+}
---
+++ front-end/src/app/services/order-line.service.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import {OrderLine} from "../models/order-line";
+import {GlobalConstant} from "../modules/shared/global-constant";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderLineService {
+  constructor() {
+  }
+
+  addOrderLineToOrderLocalStorage(order: OrderLine[], orderLine: OrderLine): void {
+      order.push(orderLine);
+      localStorage.setItem(GlobalConstant.ORDER, JSON.stringify(order));
+  }
+
+  getCurrentOrderLines(): OrderLine[] {
+    return JSON.parse(localStorage.getItem(GlobalConstant.ORDER));
+  }
+}
