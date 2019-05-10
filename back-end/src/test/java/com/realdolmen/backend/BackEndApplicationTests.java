eb3a05f7cef987e51c20e97828ebf854d3f1eaec
thomasRD08 <44640015+thomasRD08@users.noreply.github.com>
2019-05-10 17:16:09 +0200

    remove profiles

--- back-end/src/test/java/com/realdolmen/backend/BackEndApplicationTests.java
+++ back-end/src/test/java/com/realdolmen/backend/BackEndApplicationTests.java
@@ -1,4 +1,4 @@
-package com.realdolmen.backend;
+/*package com.realdolmen.backend;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,4 +13,4 @@ public class BackEndApplicationTests {
     @Test
     public void testShouldCheckContextLoads() {
     }
-}
+}*/
